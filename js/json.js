5b947c17409eec68ac857cadc9085eb8ea54f315
Nvvme <navan2400@gmail.com>
2024-03-20 10:41:39 -0400

    finished populateHeader()

--- js/json.js
+++ js/json.js
@@ -6,7 +6,7 @@ const section = document.querySelector("section");
 async function populate() {
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
-    const requestURL = "https://nvvme.github.io/comp1073Lesson10/js/i-scream.json";
+    const requestURL = "https://nvvme.github.io/comp1073Lesson10/";
     // STEP 5: Use the new URL to create a new request object
     const request = new Request(requestURL);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
@@ -15,9 +15,8 @@ async function populate() {
     const iScream = await response.json();
     // STEP 8: Output the iScream JSON object to the console 
     console.log(iScream);
-    
     // STEP 9a: Invoke the populateHeader function here, then build it below
-    
+    populateHeader(iScream);
     // STEP 10a: Invoke the showTopFlavors function here, then build it below
     
 }
@@ -26,13 +25,13 @@ async function populate() {
 populate();
 
 /* STEP 9b: Build out the populateHeader() function */
-function populateHeader() {
+function populateHeader(jsonObj) {
     // Create the H1 element
-    
+    let headerH1 = document.createElement("h1");
     // Grab the company name from the JSON object and use it for the text node
-    
+    headerH1.textContent = jsonObj.companyName;
     // Inject the complete H1 element into the DOM, inside the HEADER
-    
+    header.appendChild(headerH1);
 };
 /* STEP 10b: Assemble the showTopFlavors() function */
 function showTopFlavors() {
